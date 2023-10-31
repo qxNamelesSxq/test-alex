efcaf1445242e048557b8ff7254e6b280e2ae9d3
xNamelesSx <sanya.chepelev.97@gmail.com>
2023-10-31 09:42:31 +0200

    set up a basic js project, added commitlint and husky for liniting commit messages

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => (
   <div>
     <h1>My React App</h1>
-    <p>sqq</p>
+
     <p>sss</p>
     <p>This is a simple React app written in TypeScript.</p>
   </div>
