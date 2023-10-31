8d99a9ff7eeb68ac5b4ac6741fe364115da510cd
xNamelesSx <sanya.chepelev.97@gmail.com>
2023-10-31 09:51:13 +0200

    ci: initialised basic js project, added commitlint and husky to lint commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,24 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'button',
+      ],
+    ],
+  },
 };
