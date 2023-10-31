b9de8203ac831ec2678a91125bf5805d44ec468e
xNamelesSx <sanya.chepelev.97@gmail.com>
2023-10-31 10:01:00 +0200

    ci: second, test

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const App = () => (
   <div>
-    <h1>My React App</h1>
     <h2>sss</h2>
     <h3>sdqw</h3>
     <p>This is a simple React app written in TypeScript.</p>
